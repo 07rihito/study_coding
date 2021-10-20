d525ef91613081ad0dab35a8735d96330f72edb2
07rihito <onashu.ganbaruzoi@gmail.com>
2021-10-20 21:57:49 +0900

    [modify]ABC200C python code

--- ABC/200/C/ans.py
+++ ABC/200/C/ans.py
@@ -1,6 +1,11 @@
 n = int(input())
 
 num_list = list(map(int, input().split()))
+count = 0
 
-for i in range(n):
-  for i+1 in range():
+for i in range(0, n):
+  for j in range(i+1, n):
+    if (num_list[i]-num_list[j]) % 200 == 0:
+      count += 1
+
+print(int(count))
